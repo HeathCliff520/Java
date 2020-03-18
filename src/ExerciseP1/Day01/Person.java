735b9d1c1de386a5f7772dd6080d91e9853ee085
HeathCliff520 <40429478+HeathCliff520@users.noreply.github.com>
2020-03-18 14:19:39 +0800

    exercise from net tutorial of HeiMa company

---
+++ src/ExerciseP1/Day01/Person.java
@@ -0,0 +1,31 @@
+package ExerciseP1.Day01;
+
+public class Person {
+    public Person(String name, int age) {
+        this.name = name;
+        this.age = age;
+    }
+
+    public Person() {
+    }
+
+    public String getName() {
+        return name;
+    }
+
+    public int getAge() {
+        return age;
+    }
+
+    public void setName(String name) {
+        this.name = name;
+    }
+
+    public void setAge(int age) {
+        this.age = age;
+    }
+
+    String name;
+    int age;
+
+}
---
+++ src/ExerciseP1/Day01/PersonMain.java
@@ -0,0 +1,8 @@
+package ExerciseP1.Day01;
+
+public class PersonMain {
+    public static void main(String[] args) {
+        Person p1=new Person("小米",25);
+        System.out.println(p1.name);
+    }
+}
