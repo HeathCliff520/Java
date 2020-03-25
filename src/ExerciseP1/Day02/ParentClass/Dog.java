cb46086faf7f20431ee83f790a2a116ed9ab4856
HeathCliff520 <40429478+HeathCliff520@users.noreply.github.com>
2020-03-25 19:33:08 +0800

    learning parent class

--- src/ExerciseP1/Day02/ParentClass/Dog.java
+++ src/ExerciseP1/Day02/ParentClass/Dog.java
@@ -3,6 +3,7 @@ package ExerciseP1.Day02.ParentClass;
 public class Dog extends Animal {
     String DogName;
     static String NameOfThisClass = "狗";
+    int age;
     public Dog() {
        //super()函数是父类的空构造
         System.out.println("这里默认调用了super()函数");
@@ -12,6 +13,9 @@ public class Dog extends Animal {
         DogName = Name;
     }
 
+    public void ageover(){
+
+    }
     //重写父类中的抽象方法
     @Override
     public void GoMethod() {
